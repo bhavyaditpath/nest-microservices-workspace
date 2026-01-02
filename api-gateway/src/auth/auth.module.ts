6db029e687b2f4281be075e9b88b199b9e80be50
bhavyaditpath <bhavyad.itpath@gmail.com>
2026-01-02 10:06:40 +0530

    added auth in the API_Gateway

---
+++ api-gateway/src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'JWT_SECRET_KEY',
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [JwtModule],
+})
+export class AuthModule {}
