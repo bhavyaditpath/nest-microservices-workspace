bda67e59a41229d00c552a7e70c6b3d696532a44
bhavyaditpath <bhavyad.itpath@gmail.com>
2026-01-07 11:29:50 +0530

    remove Auht files from the inventory service which is not needed

--- inventory-service/src/app.module.ts
+++ inventory-service/src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InventoryModule } from './inventory/inventory.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     InventoryModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
