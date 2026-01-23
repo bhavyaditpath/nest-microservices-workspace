8c4d2c0f057fc2b8435a757f532633f0c2a23132
bhavyaditpath <bhavyad.itpath@gmail.com>
2026-01-23 11:38:50 +0530

    Add TypeORM migration scripts and update tsconfig paths for shared modules

--- api-gateway/src/alert/alert.module.ts
+++ api-gateway/src/alert/alert.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AlertController } from './alert.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [AlertController],
 })
 export class AlertModule {}
